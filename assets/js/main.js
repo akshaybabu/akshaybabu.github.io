7617d3d6205a4b4ed2be242ca61285d4b3ffbcb7
Akshay Babu Anthoorveettil <anthoorveettilakshaybabu@gmail.com>
2024-08-17 17:37:58 +0530

    Updated
    Updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -149,3 +149,17 @@
   });
 
 })(jQuery);
+document.querySelectorAll('.accordion-header').forEach(header => {
+  header.addEventListener('click', () => {
+    const item = header.parentElement;
+    const content = item.querySelector('.accordion-content');
+
+    item.classList.toggle('active');
+
+    if (item.classList.contains('active')) {
+      content.style.display = 'block';
+    } else {
+      content.style.display = 'none';
+    }
+  });
+});
